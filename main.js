8dbe9501539f136f5f4da08f8a620aaa1eb3bdc9
Jassinger123 <104770712+Jassinger123@users.noreply.github.com>
2022-07-08 11:03:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=create_Canvas(700,600)
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save=("myimage.jpeg");
+}
